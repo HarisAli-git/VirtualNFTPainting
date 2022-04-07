29a7449d11610177b7dc23ca2c5417fd0bb8db64
HarisAli-git <harisa73@gmail.com>
2022-04-07 23:39:06 +0500

    Added Tags in post - Backend

--- BackEnd/app/routes/tags.js
+++ BackEnd/app/routes/tags.js
@@ -5,5 +5,6 @@ const TagsController = require("../controllers/tags");
 
 router.post("/addtag", TagsController.create_tag);
 router.get("/", TagsController.view_tag);
+router.get("/post", TagsController.get_post_tag);
 
 module.exports = router;
